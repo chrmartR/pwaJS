6a42bea6368df3d91d95d835043c20b5facba510
Christopher Martin <chrmart@nus11789-11-chrmart.nueva.local>
2019-02-15 10:10:33 -0800

    updated parsing

--- parseOSM.js
+++ parseOSM.js
@@ -1,78 +1,43 @@
-var fs = require('fs'),
-    xml2js = require('xml2js');
-var parser = new xml2js.Parser();
 var nodeDict = {};
 var allWays = [];
 var nodesF = [];
 var waysF = [];
-fs.readFile('mapFiles/nuevaArea.xml', function(err, data) {
-    parser.parseString(data, function (err, r) {
-      //RECORD ALL THE NODES
-      for (var index in r.osm.node) {
-          var nodeID = r.osm.node[index].$.id;
-          var nodeLat = r.osm.node[index].$.lat;
-          var nodeLon = r.osm.node[index].$.lon;
-          if(nodeDict[nodeID]===undefined){
-            nodeDict[nodeID] = [nodeLat, nodeLon];
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+};
+xhttp.open("GET", "nuevaArea.xml", true);
+xhttp.send();
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    var waysTemp = xmlDoc.getElementsByTagName("way");
+    var filteredWaysDict = {};
+    for(i = 0; i < waysTemp.length; i++){
+      tags = waysTemp[i].getElementsByTagName("tag");
+      for(j = 0; j < tags.length; j++){
+        var attributeKey = tags[j].attributes.k.nodeValue;
+        if(attributeKey==="highway"){
+          if(tags[j].attributes.v.nodeValue.indexOf("motorway")===-1){
+            filteredWaysDict[waysTemp[i].id] = waysTemp[i];
+            break;
           }
-      }
-      console.log('Nodes Gathered');
-      //RECORD ALL THE WAYS
-      for (var index in r.osm.way) {
-          var waytags = r.osm.way[index].tag;
-          if(waytags == undefined){
-            continue;
-          }
-          var isHighway = false;
-          var isMotorway = false;
-          for (var tagIndex in waytags) {
-            if (waytags[tagIndex].$.k == "highway"){
-              isHighway = true;
-              roadType = waytags[tagIndex].$.v;
-              if (roadType.includes("motorway")){
-                isMotorway = true;
-              }
-            }
-          }
-          if(!isHighway||isMotorway){
-            continue;
-          }
-          allWays.push(r.osm.way[index]);
-
-      }
-      console.log('Ways Gathered');
-      //PROCESS AND CREATE GRID
-      var nodeIDList = [];
-      for (var windex in allWays) {
-        var currentWay = allWays[windex]; //get way
-        var wayNodeList = currentWay.nd; //get list of nodes in way
-        var lastNode;
-        for (var nindex in wayNodeList) { //iterate through nodes
-          var cNode;
-          var cWayNode = wayNodeList[nindex]; //find node in way format, only id
-          var cNodeId = cWayNode.$.ref; //find id number
-          var cNodeIndex = nodeIDList.indexOf(cNodeId);
-          if(cNodeIndex==-1){ //If not created before, create and add to list
-            var cNodeLatLn = nodeDict[cNodeId]; //find coordinates
-            cNode = new node(cNodeLatLn[0],cNodeLatLn[1],cNodeId);
-            nodeIDList.push(cNodeId);
-            nodesF.push(cNode);
-          }
-          else{ //If created before, pull out of list
-            cNode = nodesF[cNodeIndex]; //use dictionary later
-          }
-          if(nindex == 0){
-            lastNode = cNode;
-            continue;
-          }
-          var nodeDistance = getDistance(lastNode.latitude,lastNode.longitude,cNode.latitude,cNode.longitude);
-          var newPath = new path(lastNode, cNode, nodeDistance);
-          lastNode.edges.push(newPath);
-          cNode.edges.push(newPath);
-          waysF.push(newPath);
         }
       }
-      console.log('Paths, Nodes Created');
-      console.log(waysF)
-    });
-});
+    }
+    var nodesTemp = xmlDoc.getElementsByTagName("node");
+    var nodesDict = {};
+    for(i = 0; i < nodesTemp.length; i++){
+      nodesDict[nodesTemp[i].id] = nodesTemp[i];
+    }
+    console.log(filteredWaysDict[Object.keys(filteredWaysDict)[0]].children[0]);
+    for(i = 0; i < Object.keys(filteredWaysDict).length; i++){
+      var currentWay = filteredWaysDict[Object.keys(filteredWaysDict)[i]];
+      var connectedNodes = [];
+      for(j = 0; j < children.length; j++){
+        
+      }
+
+    }
+}
