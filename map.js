9223bd70bb8c178a29931c996d5c2ee80b6f904e
Christopher Martin <chrmart@nus11789-11-chrmart.nueva.local>
2019-02-27 09:54:32 -0800

    updated path generator

--- map.js
+++ map.js
@@ -1,15 +1,48 @@
-var mymap = L.map('mapid').setView([37.561959,-122.325554], 13);
+var map = L.map('mapid').setView([37.561959,-122.325554], 13);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(mymap);
-var latlngs = [[1,1],[2,2],[3,3]];
+    }).addTo(map);
+var latlngs = [[37,-122],[38,-122],[38,-123],[37,-123]];
 var markers = [];
-var polyline = L.polyline(latlngs, {color: '#5100B5', opacity: 0.5}).addTo(mymap);
+var polyline = L.polyline(latlngs, {color: '#5100B5', opacity: 0.5}).addTo(map);
+var PATHDISTANCE = 10000;
+function resetNodes(){
+  for (var nodeTag in nodeDict) {
+    nodeDict[nodeTag].visited = false;
+  }
+}
+function initHeuristicDistance(startingNode){
+  for (var nodeTag in nodeDict) {
+    nodeDict[nodeTag].distance = getDistance(startingNode.latitude, startingNode.longitude, nodeDict[nodeTag].latitude, nodeDict[nodeTag].longitude);
+  }
+}
 function onMapClick(e) {
-    var marker = L.marker(e.latlng).addTo(mymap);
-    latlngs.push(e.latlng);
-    markers.push(marker);
-    polyline.setLatLngs(latlngs);
+  var shortestDistance = Infinity;
+  var closestNode;
+  for (var nodeTag in nodeDict) {
+    var distanceFromClick = getDistance(e.latlng.lat, e.latlng.lng, nodeDict[nodeTag].latitude, nodeDict[nodeTag].longitude);
+    if(distanceFromClick<shortestDistance){
+      shortestDistance = distanceFromClick;
+      closestNode = nodeDict[nodeTag];
+    }
+  }
+  var marker = L.marker([closestNode.latitude,closestNode.longitude]).addTo(map);
+  latlngs.push([closestNode.latitude,closestNode.longitude]);
+  markers.push(marker);
+  polyline.setLatLngs(latlngs);
+}
+function chooseNeighborNode(currentNode, neighborEdges){
+  var furthestDistance = -1;
+  var neighborEdge;
+  for (var index in neighborEdges){
+    var currentNeighbor = neighborEdges[index].getOther(currentNode);
+    if(currentNeighbor.distance>furthestDistance){
+      furthestDistance = currentNeighbor.distance;
+      neighborEdge = neighborEdges[index];
+    }
+  }
+  neighborNode = neighborEdge.getOther(currentNode);
+  return [neighborNode, neighborEdge];
 }
 function onKeyPress(e) {
     if(e.originalEvent.key==="q"){  //remove last marker from the map
@@ -26,6 +59,46 @@ function onKeyPress(e) {
       latlngs = [];
       polyline.setLatLngs(latlngs);
     }
+    if(e.originalEvent.key==="g"){
+      resetNodes();
+      while(markers.length>0){
+        markers.pop().remove();
+      }
+      latlngs = [];
+      var startingNode = nodeDict["564663890"];
+      initHeuristicDistance(startingNode);
+      var path = [];
+      var distanceTraveled = 0;
+      var currentNode = startingNode;
+      while(distanceTraveled<PATHDISTANCE){
+        currentNode.visited = true;
+        path.push([currentNode.latitude, currentNode.longitude]);
+        var neighborEdgesUnvisited = [];
+        for(i = 0; i < currentNode.edges.length; i++){
+          var edgeNode = currentNode.edges[i].getOther(currentNode);
+          if(!edgeNode.visited){
+            neighborEdgesUnvisited.push(currentNode.edges[i]);
+          }
+        }
+        var neighborNode;
+        if(neighborEdgesUnvisited.length>0){
+          neighborData = chooseNeighborNode(currentNode, neighborEdgesUnvisited);
+          neighborNode = neighborData[0];
+          neighborEdge = neighborData[1];
+        }
+        else{
+          var neighborEdge = currentNode.edges[Math.floor(Math.random() * currentNode.edges.length)];
+          var neighborNode = neighborEdge.getOther(currentNode);
+        }
+        distanceTraveled = distanceTraveled + neighborEdge.distance;
+        currentNode = neighborNode;
+      }
+      for(i = 0; i < path.length; i++){
+        latlngs.push(path[i]);
+        markers.push(L.marker(path[i]).addTo(map));
+      }
+      polyline.setLatLngs(latlngs);
+    }
 }
-mymap.on('click', onMapClick);
-mymap.on('keypress', onKeyPress);
+map.on('click', onMapClick);
+map.on('keypress', onKeyPress);
--- parseOSM.js
+++ parseOSM.js
@@ -1,7 +1,52 @@
+function getDistance(lat1,lon1,lat2,lon2) { //distance between two points on the earth based on lat and lon
+  var R = 6356.752; // Radius of the earth in km
+  var dLat = d2r(lat2-lat1);  // deg2rad below
+  var dLon = d2r(lon2-lon1);
+  var a =
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(d2r(lat1)) * Math.cos(d2r(lat2)) *
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c; // Distance in km
+  return d * 1000; //distance in m
+}
+
+function d2r(deg) {
+  return deg * (Math.PI/180)
+}
+function Node(lat,lon,num){
+  this.latitude = lat;
+  this.longitude = lon;
+  this.idNum = num;
+  this.edges = [];
+  this.distance = 0;
+  this.parent = null;
+  this.visited = false;
+  this.rootDistance = 0;
+  this.targetDistance = 0;
+  this.heuristicNum = 0;
+}
+function Edge(n1,n2,l){
+  this.node1 = n1;
+  this.node2 = n2;
+  this.distance = l;
+  this.getOther = function(n) {
+    if(n.idNum === this.node1.idNum) {
+      return this.node2;
+    }
+    else if(n.idNum === this.node2.idNum) {
+      return this.node1;
+    }
+    else {
+      return 0;
+    }
+  }
+}
 var nodeDict = {};
 var allWays = [];
 var nodesF = [];
-var waysF = [];
+var edgesF = [];
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -27,17 +72,41 @@ function myFunction(xml) {
       }
     }
     var nodesTemp = xmlDoc.getElementsByTagName("node");
-    var nodesDict = {};
-    for(i = 0; i < nodesTemp.length; i++){
-      nodesDict[nodesTemp[i].id] = nodesTemp[i];
+    var initialNodeDict = {};
+    for(i = 0; i < nodesTemp.length; i++){ //Create a dictionary of all nodes on the map
+      var currentNodeObject = new Node(nodesTemp[i].attributes.lat.nodeValue, nodesTemp[i].attributes.lon.nodeValue, nodesTemp[i].id);
+      initialNodeDict[nodesTemp[i].id] = currentNodeObject;
     }
-    console.log(filteredWaysDict[Object.keys(filteredWaysDict)[0]].children[0]);
+    for(i = 0; i < Object.keys(filteredWaysDict).length; i++){
+      var currentWay = filteredWaysDict[Object.keys(filteredWaysDict)[i]];
+      for(j = 0; j < currentWay.children.length; j++){
+        if(currentWay.children[j].nodeName==="nd"){
+          if(typeof nodeDict[currentWay.children[j].attributes[0].nodeValue] === "undefined"){
+            var currentWayNode = currentWay.children[j];
+            var currentWayNodeId = currentWayNode.attributes[0].nodeValue;
+            nodeDict[currentWayNodeId] = new Node(initialNodeDict[currentWayNodeId].latitude,initialNodeDict[currentWayNodeId].longitude,currentWayNodeId);
+          }
+        }
+      }
+    }
+    delete initialNodeDict;
     for(i = 0; i < Object.keys(filteredWaysDict).length; i++){
       var currentWay = filteredWaysDict[Object.keys(filteredWaysDict)[i]];
       var connectedNodes = [];
-      for(j = 0; j < children.length; j++){
-        
+      for(j = 0; j < currentWay.children.length; j++){
+        if(currentWay.children[j].nodeName==="nd"){
+          connectedNodes.push(currentWay.children[j].attributes[0].nodeValue);
+        }
       }
-
+      //ConnectedNodes addition
+      for(j = 0; j < connectedNodes.length - 1; j++){
+        var node1 = nodeDict[connectedNodes[j]];
+        var node2 = nodeDict[connectedNodes[j+1]];
+        var nodeDistance = getDistance(node1.latitude, node1.longitude, node2.latitude, node2.longitude);
+        var newEdge = new Edge(node1, node2, nodeDistance);
+        node1.edges.push(newEdge);
+        node2.edges.push(newEdge);
+        edgesF.push(newEdge);
+      }
     }
 }
